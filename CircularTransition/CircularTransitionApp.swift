ee604a76c269ba965d1bd03991f79267555e8bc9
Raphael Cerqueira <raphaelfc@gmail.com>
2021-05-08 18:03:09 -0300

    Initial Commit

---
+++ CircularTransition/CircularTransitionApp.swift
@@ -0,0 +1,17 @@
+//
+//  CircularTransitionApp.swift
+//  CircularTransition
+//
+//  Created by Raphael Cerqueira on 08/05/21.
+//
+
+import SwiftUI
+
+@main
+struct CircularTransitionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
