19f5ebd79be28a9d1cf4579fbe38cf95431f6888
Raphael Cerqueira <raphaelfc@gmail.com>
2021-05-08 19:13:01 -0300

    Primeiro commit

--- CircularTransition/ContentView.swift
+++ CircularTransition/ContentView.swift
@@ -8,9 +8,76 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var showSecondView = false
+    @Namespace var animation
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Text("Hello, world!")
+                .padding()
+            
+            VStack {
+                Spacer()
+                
+                HStack {
+                    Spacer()
+                    
+                    Button(action: {
+                        withAnimation {
+                            showSecondView.toggle()
+                        }
+                    }, label: {
+                        ZStack {
+                            Circle()
+                                .matchedGeometryEffect(id: "background", in: animation)
+                                .frame(width: 60, height: 60)
+                            
+                            Image(systemName: "plus")
+                                .foregroundColor(.white)
+                        }
+                    })
+                }
+                .padding()
+            }
+            
+            if showSecondView {
+                SecondView(isVisible: $showSecondView, animation: animation)
+            }
+        }
+    }
+}
+
+struct SecondView: View {
+    @Binding var isVisible: Bool
+    let screen = UIScreen.main.bounds
+    var animation: Namespace.ID
+    
+    var body: some View {
+        ZStack {
+            Circle()
+                .fill(Color.blue)
+                .matchedGeometryEffect(id: "background", in: animation)
+                .frame(width: calculateRadius(), height: calculateRadius())
+                .padding(.leading, 400)
+                .padding(.top, 400)
+            
+            Button(action: {
+                withAnimation {
+                    isVisible.toggle()
+                }
+            }, label: {
+                Text("Close")
+                    .foregroundColor(.white)
+            })
+        }
+    }
+    
+    func calculateRadius() -> CGFloat {
+        let sideOne = screen.width / 2
+        let sideTwo = screen.height / 2
+        let aux = (sideOne * sideOne) + (sideTwo * sideTwo)
+        let radius = sqrt(aux)
+        return radius * 2
     }
 }
 
---
+++ README.md
@@ -0,0 +1,24 @@
+# Transição circular usando SwiftUI 
+Decidi implementar somente essa transição que usei no aplicativo da Gol para poder explicar em vídeo a lógica do cálculo usando o Teorema de Pitágoras.
+
+## Screenshots
+<p align="center">
+    <img src="https://user-images.githubusercontent.com/16376748/117554902-eca3ab00-b030-11eb-8250-c4c55e34e63e.png" width="400"> &nbsp;
+    <img src="https://user-images.githubusercontent.com/16376748/117554905-f2998c00-b030-11eb-8774-6430c0fce6f7.png" width="400"> &nbsp;
+</p>
+
+## Vídeos
+- [Vídeo no YouTube](https://youtu.be/qPiu9kLRjDk)
+
+## Requisitos
+- iOS 14
+- SwiftUI
+- Xcode 12
+
+## Meta
+- Github: [https://github.com/rphlfc](https://github.com/rphlfc)
+- Instagram: [https://www.instagram.com/rphlfc.dev](https://www.instagram.com/rphlfc.dev)
+- LinkedIn: [http://www.linkedin.com/in/rphlfc](http://www.linkedin.com/in/rphlfc)
+- Twitter: [https://twitter.com/rphlfc](https://twitter.com/rphlfc)
+- Website: [https://rphlfc.github.io](https://rphlfc.github.io)
+- Youtube: [https://www.youtube.com/c/raphaelcerqueira](https://www.youtube.com/c/raphaelcerqueira)
